8927f063181207b26bf3a80191eac0aebe9f665f
weiwentan23 <weiwentan@shimmersensing.com>
2023-07-20 09:27:40 +0800

    first

---
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnGet()
+        {
+            using (StreamReader r = new StreamReader("./wwwroot/json/data.json"))
+            {
+                string json = r.ReadToEnd();
+                TempData["data"] = JsonConvert.SerializeObject(json);
+            }
+
+            using (StreamReader r = new StreamReader("./wwwroot/json/data2.json"))
+            {
+                string json = r.ReadToEnd();
+                TempData["data2"] = JsonConvert.SerializeObject(json);
+            }
+            return Page();
+        }
+    }
+}
---
+++ WebApplication1/Pages/Profile.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class ProfileModel : PageModel
+    {
+        private readonly ILogger<ProfileModel> _logger;
+
+        public ProfileModel(ILogger<ProfileModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
