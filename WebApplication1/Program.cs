7737f98e5c2211ea883686f32a2d63b793276f65
weiwentan23 <weiwentan@verisense.net>
2026-08-24 23:39:00 +0800

    test2

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
-//commit1 test
+//commit1 test commit2 test
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
